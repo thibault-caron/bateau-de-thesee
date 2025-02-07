f5b269723a7af737bb6a68797876a2e380343c7a
Thibault Caron <thibault.caron@laplateforme.io>
2025-02-07 11:09:05 +0100

    feat: first step, classes Part and Ship

---
+++ bateau_thesee_v1.py
@@ -0,0 +1,26 @@
+class Part:
+    def __init__(self, name, material):
+        self.name = name
+        self.material = material
+
+    def change_material(self, new_material):
+        self.material = new_material
+
+    def __str__(self):
+        return f"part name: {self.name}\npart material: {self.material}"
+    
+
+class Ship(Part):
+    def __init__(self, name, material, name_ship, parts={}):
+        super().__init__(name, material)
+        self.__name_ship = name_ship
+        self.__parts = {part.name for part in parts}
+
+    def display_state(self):
+        for part in self.__parts.values():
+            print(f"part: {part}")
+    
+    def replace_part(self, part_name, new_part):
+        self.name = part_name
+        self.__parts[part_name] = new_part
+        return self.__parts
